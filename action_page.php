5283b32f8463c5b1c7ed8d4965d7a446bdaf2623
Vineeta <49141391+Vineeta12345@users.noreply.github.com>
2020-06-13 22:57:35 +0530

    Add files via upload

--- action_page.php
+++ action_page.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "vvineeta09@gmail.com";
+    $email_to = "vhsinfotech@gmail.com";
     $email_subject = "New form submissions";
 
     function problem($error)
